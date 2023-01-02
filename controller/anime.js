1ff7f284e110a6fa27cf82553607b2bdb9a6b20a
xMaulana <muhammadwisnumaulana73@gmail.com>
2023-01-02 21:43:43 +0700

    add headers

--- controller/anime.js
+++ controller/anime.js
@@ -9,12 +9,28 @@ const virtualConsole = new jsdom.VirtualConsole()
 //         "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js"
 //       }
 // })
+
+const user_agent = [
+    'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
+]
 const website = "https://otakudesu.bid"
 
 const getData = async (link)=>{
     const data = await axios.get(link, {
         headers: {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
+            "User-Agent": user_agent[Math.floor(Math.random()%user_agent.length)],
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1",
+            "Cache-Control": "max-age=0",
         }
     });
     return new JSDOM(data.data, {virtualConsole}).window.document;
