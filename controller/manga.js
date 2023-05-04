a30c947f9110e341ee5e944bdc93b34f55c16d7b
xMaulana <muhammadwisnumaulana73@gmail.com>
2023-05-04 12:51:29 +0700

    fix manga

--- controller/manga.js
+++ controller/manga.js
@@ -1,7 +1,7 @@
 const {JSDOM} = require("jsdom");
 const axios = require('axios');
 
-const website = "https://komikindo.id/";
+const website = "https://komikindo.pro/";
 
 const user_agent = [
     'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
